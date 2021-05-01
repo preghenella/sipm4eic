f9013447d786b484312e5e90245be6991c191b15
Roberto Preghenella <preghenella@bo.infn.it>
2021-05-01 07:30:46 +0200

    The macro that makes the IV curve

---
+++ characterisation/macros/makeiv.C
@@ -0,0 +1,53 @@
+#include "graphutils.C"
+
+TGraphErrors *
+makeiv(const std::string fnivscan, const std::string fnzero, bool invertX = true, bool invertY = true)
+{
+
+  /** compute zero volt level **/
+  auto zero = 0.;
+  auto ezero = 0.;
+  if (!fnzero.empty()) {
+    std::cout << " --- open zero volt level file: " << fnzero << std::endl;
+    auto gzero = new TGraph(fnzero.c_str(), "%lg %*lg %lg");
+    gzero = invertY ? graphutils::invertY(gzero) : gzero;
+    auto pzero = new TProfile("pzero", "", 1, 0., 1.);
+    for (int i = 0; i < gzero->GetN(); ++i)
+      pzero->Fill(0., gzero->GetY()[i]);
+    zero = pzero->GetBinContent(1);
+    ezero = pzero->GetBinError(1);
+  }
+  std::cout << " --- zero volt level: " << zero * 1.e12 << " +- " << ezero * 1.e12 << " pA" << std::endl;
+
+  /** IV curve from average of all measurements **/
+  std::cout << " --- open iv scan file: " << fnivscan << std::endl;
+  auto givscan = new TGraph(fnivscan.c_str(), "%*lg %lg %lg");
+  givscan = invertX ? graphutils::invertX(givscan) : givscan;
+  givscan = invertY ? graphutils::invertY(givscan) : givscan;
+  auto pivscan = new TProfile("pivscan", "", 10001, -0.005, 100.005);
+  for (int i = 0; i < givscan->GetN(); ++i)
+    pivscan->Fill(givscan->GetX()[i], givscan->GetY()[i]);
+
+  /** write as TGraphErrors **/
+  auto gout = new TGraphErrors;
+  auto npoints = 0;
+  for (int i = 0; i < pivscan->GetNbinsX(); ++i) {
+    if (pivscan->GetBinError(i + 1) == 0.) continue;
+    auto cen = pivscan->GetBinCenter(i + 1);
+    auto val = pivscan->GetBinContent(i + 1);
+    auto eval = pivscan->GetBinError(i + 1);
+    val = val - zero;
+    eval = std::sqrt(eval * eval + ezero * ezero);
+    gout->SetPoint(npoints, cen, val);
+    gout->SetPointError(npoints, 0., eval);
+    npoints++;
+  }
+  auto lastindex = fnivscan.find_last_of("."); 
+  auto fnout = fnivscan.substr(0, lastindex) + ".makeiv.root";
+  std::cout << " --- writing output file: " << fnout << std::endl;
+  auto fout = TFile::Open(fnout.c_str(), "RECREATE");
+  gout->Write("ivscan");
+  fout->Close();
+  
+  return gout;
+}
